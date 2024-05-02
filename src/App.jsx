517d64cc6b80b72fc2df371b848a4ab974477027
svivek19 <svivek030503@gmail.com>
2024-05-02 10:11:28 +0530

    logic finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 export default function App() {
   const [color, setColor] = useState("red");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (color === "red") {
+        setColor("yellow");
+      } else if (color === "yellow") {
+        setColor("green");
+      } else {
+        setColor("red");
+      }
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [color]);
+
   return (
     <div className="traffic-light">
-      <div className={`red light `}></div>
-      <div className={`yellow light `}></div>
-      <div className={`green light `}></div>
+      <div className={`red light ${color === "red" ? "active" : ""}`}></div>
+      <div
+        className={`yellow light ${color === "yellow" ? "active" : ""}`}
+      ></div>
+      <div className={`green light ${color === "green" ? "active" : ""}`}></div>
     </div>
   );
 }
