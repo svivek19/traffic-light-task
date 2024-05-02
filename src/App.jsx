b12346c269fba210fe8d13cfc6b8b4c1ed245a7d
svivek19 <svivek030503@gmail.com>
2024-05-02 10:06:21 +0530

    style finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
-const App = () => {
-  return <div>App</div>;
-};
-
-export default App;
+export default function App() {
+  const [color, setColor] = useState("red");
+  return (
+    <div className="traffic-light">
+      <div className={`red light `}></div>
+      <div className={`yellow light `}></div>
+      <div className={`green light `}></div>
+    </div>
+  );
+}
